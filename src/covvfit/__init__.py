889deb2883aead07578eff84cdb9b3116865bb75
dr-david <64543683+dr-david@users.noreply.github.com>
2023-11-10 23:46:23 +0100

    Frequentist fitting utilities and notebook (#7)
    * multinomial experiments
    
    * first tries of fitting the likelihood model
    
    * likelihood fitting
    
    * added notebook for fitting JN.1
    
    * Apply formatter
    
    * Refactor the code.
    
    * Translate multinomial notebook from ipynb into qmd
    
    * Translate frequentist notebook from ipynb into qmd
    
    ---------
    
    Co-authored-by: Paweł Czyż <pczyz@protonmail.com>

--- src/covvfit/__init__.py
+++ src/covvfit/__init__.py
@@ -1,6 +1,18 @@
 from covvfit._splines import create_spline_matrix
+from covvfit._preprocess_abundances import make_data_list, preprocess_df, load_data
+
+import covvfit._frequentist as freq
+import covvfit.plotting as plot
 
 VERSION = "0.1.0"
 
 
-__all__ = ["create_spline_matrix", "VERSION"]
+__all__ = [
+    "create_spline_matrix",
+    "make_data_list",
+    "preprocess_df",
+    "load_data",
+    "VERSION",
+    "freq",
+    "plot",
+]
