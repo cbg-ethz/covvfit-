15d416587496a20577a681c0df9f6c4589dab565
dr-david <64543683+dr-david@users.noreply.github.com>
2024-11-08 22:10:24 +0100

    Frequentist notebook (#10)
    * Add requirements file
    
    * Fix dependencies
    
    * Add annotations to create_model
    
    * Add reproducible version
    
    * added plotting utilities
    
    * notebook for fitting latest results
    
    * added notebook on how to prepare the deconv
    
    * Apply black
    
    * Add ruff

--- src/covvfit/plotting/_timeseries.py
+++ src/covvfit/plotting/_timeseries.py
@@ -1,28 +1,32 @@
 """utilities to plot"""
 import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
+import numpy as np
 import pandas as pd
 
 colors_covsp = {
     "B.1.1.7": "#D16666",
     "B.1.351": "#FF6666",
     "P.1": "#FFB3B3",
-    "B.1.617.1": "#A3FFD1",
-    "B.1.617.2": "#66C266",
+    "B.1.617.1": "#66C265",
+    "B.1.617.2": "#66A366",
     "BA.1": "#A366A3",
     "BA.2": "#CFAFCF",
-    "BA.4": "#8467F6",
-    "BA.5": "#595EF6",
-    "BA.2.75": "#DE9ADB",
-    "BQ.1.1": "#8fe000",
-    "XBB.1.9": "#dd6bff",
+    "BA.4": "#8a66ff",
+    "BA.5": "#585eff",
+    "BA.2.75": "#008fe0",
+    "BQ.1.1": "#ac00e0",
+    "XBB.1.9": "#bb6a33",
     "XBB.1.5": "#ff5656",
     "XBB.1.16": "#e99b30",
-    "XBB.2.3": "#b4b82a",
-    "EG.5": "#359f99",
+    "XBB.2.3": "#f5e424",
+    "EG.5": "#b4e80b",
     "BA.2.86": "#FF20E0",
-    "JN.1": "#00e9ff",
-    "undetermined": "#999696",
+    "JN.1": "#00e9ff",  # improv
+    "KP.2": "#D16666",  # improv
+    "KP.3": "#66A366",  # improv
+    "XEC": "#A366A3",  # improv
+    "undetermined": "#969696",
 }
 
 
@@ -58,3 +62,73 @@ def num_to_date(num, pos=None, date_min="2023-01-01", fmt="%b. '%y"):
     """convert days number into a date format"""
     date = pd.to_datetime(date_min) + pd.to_timedelta(num, "D")
     return date.strftime(fmt)
+
+
+def plot_fit(ax, ts, y_fit, variants, colors, linetype="-"):
+    """
+    Function to plot fitted values with customizable line type.
+
+    Parameters:
+        ax (matplotlib.axes): The axis to plot on.
+        ts (array-like): Time series data.
+        y_fit (array-like): Fitted values for each variant.
+        variants (list): List of variant names.
+        colors (list): List of colors for each variant.
+        linetype (str): Line style for plotting (e.g., '-', '--', '-.', ':').
+    """
+    sorted_indices = np.argsort(ts)
+    for i, variant in enumerate(variants):
+        ax.plot(
+            ts[sorted_indices],
+            y_fit[i, :][sorted_indices],
+            color=colors[i],
+            linestyle=linetype,
+            label=f"fit {variant}",
+        )
+
+
+def plot_complement(ax, ts, y_fit, variants, color="grey", linetype="-"):
+    ## function to plot 1-sum(fitted_values) i.e., the other variant(s)
+    sorted_indices = np.argsort(ts)
+    ax.plot(
+        ts[sorted_indices],
+        (1 - y_fit.sum(axis=0))[sorted_indices],
+        color=color,
+        linestyle=linetype,
+    )
+
+
+def plot_data(ax, ts, ys, variants, colors):
+    ## function to plot raw values
+    for i, variant in enumerate(variants):
+        ax.scatter(ts, ys[i, :], label="observed", alpha=0.5, color=colors[i], s=4)
+
+
+def plot_confidence_bands(
+    ax, ts, conf_bands, variants, colors, label="Confidence band", alpha=0.2
+):
+    """
+    Plot confidence intervals for fitted values on a given axis with customizable confidence level.
+
+    Parameters:
+        ax (matplotlib.axes.Axes): The axis to plot on.
+        ts (array-like): Time series data.
+        y_fit_logit (array-like): Logit-transformed fitted values for each variant.
+        logit_se (array-like): Standard errors for the logit-transformed fitted values.
+        color (str): Color for the confidence interval.
+        confidence (float, optional): Confidence level (e.g., 0.95 for 95%). Default is 0.95.
+        label (str, optional): Label for the confidence band. Default is "Confidence band".
+    """
+    # Sort indices for time series
+    sorted_indices = np.argsort(ts)
+
+    # Plot the confidence interval
+    for i, variant in enumerate(variants):
+        ax.fill_between(
+            ts[sorted_indices],
+            conf_bands["lower"][i][sorted_indices],
+            conf_bands["upper"][i][sorted_indices],
+            color=colors[i],
+            alpha=alpha,
+            label=label,
+        )
