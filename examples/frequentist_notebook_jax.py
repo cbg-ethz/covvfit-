8a46c5ce35fa79945492d088acd6244899225f18
Paweł Czyż <pczyz@protonmail.com>
2024-11-14 16:18:56 +0100

    Refactor the JAX utilities for the multinomial model (#18)
    * Sketch the refactoring
    
    * Add quasiloglikelihood
    
    * Refactor _add_variant
    
    * Remove deprecated notebook
    
    * Remove redundant imports
    
    * Mark old API constructs as deprecated.
    
    * Replace deprecated functions with new API
    
    * Rename the submodule

--- examples/frequentist_notebook_jax.py
+++ examples/frequentist_notebook_jax.py
@@ -1,16 +1,16 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py
+#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.16.4
+#       jupytext_version: 1.16.1
 #   kernelspec:
-#     display_name: jax
+#     display_name: Python 3 (ipykernel)
 #     language: python
-#     name: jax
+#     name: python3
 # ---
 
 # +
@@ -18,7 +18,6 @@ import jax
 import jax.numpy as jnp
 
 import pandas as pd
-# import pymc as pm
 
 import numpy as np
 
@@ -34,16 +33,29 @@ import yaml
 import covvfit._frequentist as freq
 import covvfit._preprocess_abundances as prec
 import covvfit.plotting._timeseries as plot_ts
-import covvfit._frequentist_jax as fj 
+
+from covvfit import quasimultinomial as qm
 
 # -
 
 
 # # Load and preprocess data
 
-DATA_PATH = "../../LolliPop/lollipop_test_noisy/deconvolved.csv"
+# +
+DATA_PATH = "../new_data/deconvolved.csv"
+VAR_DATES_PATH = "../new_data/var_dates.yaml"
+
 data = pd.read_csv(DATA_PATH, sep="\t")
 data.head()
+
+
+# Load the YAML file
+with open(VAR_DATES_PATH, "r") as file:
+    var_dates_data = yaml.safe_load(file)
+
+# Access the var_dates data
+var_dates = var_dates_data["var_dates"]
+# -
 
 data_wide = data.pivot_table(
     index=["date", "location"], columns="variant", values="proportion", fill_value=0
@@ -57,20 +69,6 @@ data_wide.head()
 max_date = pd.to_datetime(data_wide["time"]).max()
 delta_time = pd.Timedelta(days=240)
 start_date = max_date - delta_time
-# -
-
-
-
-# +
-# Path to the YAML file
-var_dates_yaml = "../../LolliPop/lollipop_test_noisy/var_dates.yaml"
-
-# Load the YAML file
-with open(var_dates_yaml, "r") as file:
-    var_dates_data = yaml.safe_load(file)
-
-# Access the var_dates data
-var_dates = var_dates_data["var_dates"]
 
 
 # +
@@ -119,35 +117,37 @@ ts_lst_scaled = [(x - t_min) / (t_max - t_min) for x in ts_lst]
 
 # +
 # %%time
-data = []
 
-for t, y in zip(ts_lst_scaled, ys_lst):
-    data.append(fj.CityData(ts=t, ys=y.T, n=1))
-        
+
 # no priors
-loss = fj.construct_total_loss(data)
-# initial parameters
-theta0 = fj.construct_theta0(n_cities=len(cities), n_variants=len(variants2))
-
-# 
-solution = fj.jax_multistart_minimize(
-    loss,
-    theta0,
-    n_starts=10
+loss = qm.construct_total_loss(
+    ys=[
+        y.T for y in ys_lst
+    ],  # Recall that the input should be (n_timepoints, n_variants)
+    ts=ts_lst_scaled,
+    average_loss=False,  # Do not average the loss over the data points, so that the covariance matrix shrinks with more and more data added
 )
 
+# initial parameters
+theta0 = qm.construct_theta0(n_cities=len(cities), n_variants=len(variants2))
+
+# Run the optimization routine
+solution = qm.jax_multistart_minimize(loss, theta0, n_starts=10)
 # -
 
 # ## Make fitted values and confidence intervals
 
 # +
 ## compute fitted values
-y_fit_lst = fj.fitted_values(ts_lst_scaled, theta=solution.x, cities=cities, n_variants=len(variants2))
+y_fit_lst = qm.fitted_values(
+    ts_lst_scaled, theta=solution.x, cities=cities, n_variants=len(variants2)
+)
 
 ## compute covariance matrix
-covariance = fj.get_covariance(loss, solution.x)
+covariance = qm.get_covariance(loss, solution.x)
 
 ## compute overdispersion
+# TODO(Pawel, David): Port compute_overdispersion to `qm`
 pearson_r_lst, overdisp_list, overdisp_fixed = freq.compute_overdispersion(
     ys_lst2, y_fit_lst, cities
 )
@@ -156,18 +156,24 @@ pearson_r_lst, overdisp_list, overdisp_fixed = freq.compute_overdispersion(
 covariance_scaled = overdisp_fixed * covariance
 
 ## compute standard errors and confidence intervals of the estimates
-standard_errors_estimates = fj.get_standard_errors(covariance_scaled)
-confints_estimates = fj.get_confidence_intervals(solution.x, standard_errors_estimates)
+standard_errors_estimates = qm.get_standard_errors(covariance_scaled)
+confints_estimates = qm.get_confidence_intervals(solution.x, standard_errors_estimates)
 
 ## compute confidence intervals of the fitted values on the logit scale and back transform
-y_fit_lst_confint = fj.get_confidence_bands_logit(solution.x, len(variants2), ts_lst_scaled, covariance_scaled)
+y_fit_lst_confint = qm.get_confidence_bands_logit(
+    solution.x, len(variants2), ts_lst_scaled, covariance_scaled
+)
 
 ## compute predicted values and confidence bands
 horizon = 60
 ts_pred_lst = [jnp.arange(horizon + 1) + tt.max() for tt in ts_lst]
 ts_pred_lst_scaled = [(x - t_min) / (t_max - t_min) for x in ts_pred_lst]
-y_pred_lst = fj.fitted_values(ts_pred_lst_scaled, theta=solution.x, cities=cities, n_variants=len(variants2))
-y_pred_lst_confint = fj.get_confidence_bands_logit(solution.x, len(variants2), ts_pred_lst_scaled, covariance_scaled)
+y_pred_lst = qm.fitted_values(
+    ts_pred_lst_scaled, theta=solution.x, cities=cities, n_variants=len(variants2)
+)
+y_pred_lst_confint = qm.get_confidence_bands_logit(
+    solution.x, len(variants2), ts_pred_lst_scaled, covariance_scaled
+)
 
 
 # -
@@ -192,10 +198,10 @@ for i, city in enumerate(cities):
     # plot fitted and predicted values
     plot_fit(ax, ts_lst[i], y_fit_lst[i], variants, colors)
     plot_fit(ax, ts_pred_lst[i], y_pred_lst[i], variants, colors, linetype="--")
-    
+
     #     # plot 1-fitted and predicted values
     plot_complement(ax, ts_lst[i], y_fit_lst[i], variants)
-#     plot_complement(ax, ts_pred_lst[i], y_pred_lst[i], variants, linetype="--")
+    #     plot_complement(ax, ts_pred_lst[i], y_pred_lst[i], variants, linetype="--")
     # plot raw deconvolved values
     plot_data(ax, ts_lst[i], ys_lst2[i], variants, colors)
     # make confidence bands and plot them
@@ -205,21 +211,22 @@ for i, city in enumerate(cities):
         ts_lst[i],
         {"lower": conf_bands[0], "upper": conf_bands[1]},
         variants,
-        colors
+        colors,
     )
-    
+
     pred_bands = y_pred_lst_confint[i]
     plot_confidence_bands(
         ax,
         ts_pred_lst[i],
         {"lower": pred_bands[0], "upper": pred_bands[1]},
         variants,
-        colors
+        colors,
     )
 
     # format axes and title
     def format_date(x, pos):
         return plot_ts.num_to_date(x, date_min=start_date)
+
     date_formatter = ticker.FuncFormatter(format_date)
     ax.xaxis.set_major_formatter(date_formatter)
     tick_positions = [0, 0.5, 1]
@@ -231,3 +238,4 @@ for i, city in enumerate(cities):
 
 fig.tight_layout()
 fig.show()
+# -
--- src/covvfit/_quasimultinomial.py
+++ src/covvfit/_quasimultinomial.py
@@ -1,11 +1,13 @@
 """Frequentist fitting functions powered by JAX."""
 
 import dataclasses
-from typing import Callable, List, NamedTuple, Optional, Sequence
+from typing import Callable, NamedTuple
 
 import jax
 import jax.numpy as jnp
 import numpy as np
+import numpyro
+import numpyro.distributions as distrib
 from jaxtyping import Array, Float
 from scipy import optimize
 
@@ -63,61 +65,12 @@ def loss(
     return -jnp.sum(n * y * logp, axis=-1)
 
 
-class CityData(NamedTuple):
-    ts: Float[Array, " timepoints"]
-    ys: Float[Array, "timepoints variants"]
-    n: _Float
-
-
 _ThetaType = Float[Array, "(cities+1)*(variants-1)"]
 
 
-def add_first_variant(vec: Float[Array, " variants-1"]) -> Float[Array, " variants"]:
-    return jnp.concatenate([jnp.zeros_like(vec)[0:1], vec])
-
-
-def construct_total_loss(
-    cities: Sequence[CityData],
-    average_loss: bool = False,
-) -> Callable[[_ThetaType], _Float]:
-    cities = tuple(cities)
-    n_variants = cities[0].ys.shape[-1]
-    for city in cities:
-        assert (
-            city.ys.shape[-1] == n_variants
-        ), "All cities must have the same number of variants"
-
-    if average_loss:  # trick for numerical stability (average loss doesnt blow up)
-        n_points_total = 1.0 * sum(city.ts.shape[0] for city in cities)
-    else:
-        n_points_total = 1.0
-
-    def total_loss(theta: _ThetaType) -> _Float:
-        rel_growths = get_relative_growths(theta, n_variants=n_variants)
-        rel_midpoints = get_relative_midpoints(theta, n_variants=n_variants)
-
-        growths = add_first_variant(rel_growths)
-        return (
-            jnp.sum(
-                jnp.asarray(
-                    [
-                        loss(
-                            y=city.ys,
-                            n=city.n,
-                            logp=calculate_logps(
-                                ts=city.ts,
-                                midpoints=add_first_variant(midp),
-                                growths=growths,
-                            ),
-                        ).sum()
-                        for midp, city in zip(rel_midpoints, cities)
-                    ]
-                )
-            )
-            / n_points_total
-        )
-
-    return total_loss
+def _add_first_variant(vec: Float[Array, " variants-1"]) -> Float[Array, " variants"]:
+    """Prepends 0 to the beginning of the vector."""
+    return jnp.concatenate((jnp.zeros(1, dtype=vec.dtype), vec))
 
 
 def construct_theta(
@@ -160,7 +113,7 @@ class StandardErrorsMultipliers(NamedTuple):
 def get_covariance(
     loss_fn: Callable[[_ThetaType], _Float],
     theta: _ThetaType,
-) -> Float[Array, "(n_params n_params)"]:
+) -> Float[Array, "n_params n_params"]:
     """Calculates the covariance matrix of the parameters.
 
     Args:
@@ -169,6 +122,12 @@ def get_covariance(
 
     Returns:
         The covariance matrix, which is the inverse of the Hessian matrix.
+
+
+    Note:
+        `loss_fn` should *not* be averaged over the data points: otherwise,
+        the covariance matrix won't shrink even when a very big data
+        set is used
     """
     hessian_matrix = jax.hessian(loss_fn)(theta)
     covariance_matrix = jnp.linalg.inv(hessian_matrix)
@@ -178,7 +137,7 @@ def get_covariance(
 
 def get_standard_errors(
     covariance: Float[Array, "n_inputs n_inputs"],
-    jacobian: Optional[Float[Array, "*output_shape n_inputs"]] = None,
+    jacobian: Float[Array, "*output_shape n_inputs"] | None = None,
 ) -> Float[Array, " *output_shape"]:
     """Delta method to calculate standard errors of a function
     from `n_inputs` to `output_shape`.
@@ -234,7 +193,7 @@ def get_confidence_intervals(
 
 
 def fitted_values(
-    times: List[Float[Array, " timepoints"]],
+    times: list[Float[Array, " timepoints"]],
     theta: _ThetaType,
     cities: list,
     n_variants: int,
@@ -290,10 +249,10 @@ def create_logit_predictions_fn(
 def get_confidence_bands_logit(
     solution_x: Float[Array, " (cities+1)*(variants-1)"],
     variants_count: int,
-    ts_lst_scaled: List[Float[Array, " timepoints"]],
+    ts_lst_scaled: list[Float[Array, " timepoints"]],
     covariance_scaled: Float[Array, "n_params n_params"],
     confidence_level: float = 0.95,
-) -> List[tuple]:
+) -> list[tuple]:
     """Computes confidence intervals for logit predictions using the Delta method,
     back-transforms them to the linear scale
 
@@ -353,7 +312,7 @@ def get_relative_advantages(theta, n_variants: int):
     # over the 0th variant
     rel_growths = get_relative_growths(theta, n_variants=n_variants)
 
-    growths = jnp.concatenate((jnp.zeros(1, dtype=rel_growths.dtype), rel_growths))
+    growths = _add_first_variant(rel_growths)
     diffs = growths[None, :] - growths[:, None]
     return diffs
 
@@ -362,10 +321,10 @@ def get_softmax_predictions(
     theta: _ThetaType, n_variants: int, city_index: int, ts: Float[Array, " timepoints"]
 ) -> Float[Array, "timepoints variants"]:
     rel_growths = get_relative_growths(theta, n_variants=n_variants)
-    growths = add_first_variant(rel_growths)
+    growths = _add_first_variant(rel_growths)
 
     rel_midpoints = get_relative_midpoints(theta, n_variants=n_variants)
-    midpoints = add_first_variant(rel_midpoints[city_index])
+    midpoints = _add_first_variant(rel_midpoints[city_index])
 
     y_linear = calculate_linear(
         ts=ts,
@@ -446,3 +405,305 @@ def jax_multistart_minimize(
         fun=solutions[optimal_index].fun,
         runs=solutions,
     )
+
+
+class _ProblemData(NamedTuple):
+    """Internal representation of the data used
+    to efficiently construct the quasilikelihood.
+
+    Attrs:
+        ts: array of shape (cities, timepoints)
+            which is padded with 0 for days where
+            there is no measurement for a particular city
+        ys: array of shape (cities, timepoints, variants)
+            which is padded with the vector (1/variants, ..., 1/variants)
+            for timepoints where there is no measurement for a particular city
+        mask: array of shape (cities, timepoints) with 0 when there is
+            no measurement for a particular city and 1 otherwise
+        n_quasimul: quasimultinomial number of trials for each city
+        overdispersion: overdispersion factor for each city
+    """
+
+    n_cities: int
+    n_variants: int
+    ts: Float[Array, "cities timepoints"]
+    ys: Float[Array, "cities timepoints variants"]
+    mask: Float[Array, "cities timepoints"]
+    n_quasimul: Float[Array, " cities"]
+    overdispersion: Float[Array, " cities"]
+
+
+def _validate_and_pad(
+    ys: list[jax.Array],
+    ts: list[jax.Array],
+    ns_quasimul: Float[Array, " cities"] | list[float] | float = 1.0,
+    overdispersion: Float[Array, " cities"] | list[float] | float = 1.0,
+) -> _ProblemData:
+    """Validation function, parsing the input provided in
+    the format convenient for the user to the internal
+    representation compatible with JAX."""
+    # Get the number of cities
+    n_cities = len(ys)
+    if len(ts) != n_cities:
+        raise ValueError(f"Number of cities not consistent: {len(ys)} != {len(ts)}.")
+
+    # Create arrays representing `n` and `overdispersion`
+    if hasattr(ns_quasimul, "__len__"):
+        if len(ns_quasimul) != n_cities:
+            raise ValueError(
+                f"Provided `ns_quasimul` has length {len(ns_quasimul)} rather than {n_cities}."
+            )
+    if hasattr(overdispersion, "__len__"):
+        if len(overdispersion) != n_cities:
+            raise ValueError(
+                f"Provided `overdispersion` has length {len(overdispersion)} rather than {n_cities}."
+            )
+
+    out_n = jnp.asarray(ns_quasimul) * jnp.ones(n_cities, dtype=float)
+    out_overdispersion = jnp.asarray(overdispersion) * jnp.ones_like(out_n)
+
+    # Get the number of variants
+    n_variants = ys[0].shape[-1]
+    for i, y in enumerate(ys):
+        if y.ndim != 2:
+            raise ValueError(f"City {i} has {y.ndim} dimension, rather than 2.")
+        if y.shape[-1] != n_variants:
+            raise ValueError(
+                f"City {i} has {y.shape[-1]} variants rather than {n_variants}."
+            )
+
+    # Ensure that the number of timepoints is consistent
+    max_timepoints = 0
+    for i, (t, y) in enumerate(zip(ts, ys)):
+        if t.ndim != 1:
+            raise ValueError(
+                f"City {i} has time axis with dimension {t.ndim}, rather than 1."
+            )
+        if t.shape[0] != y.shape[0]:
+            raise ValueError(
+                f"City {i} has timepoints mismatch: {t.shape[0]} != {y.shape[0]}."
+            )
+
+        max_timepoints = t.shape[0]
+
+    # Now create the arrays representing the data
+    out_ts = jnp.zeros((n_cities, max_timepoints))  # Pad with zeros
+    out_mask = jnp.zeros((n_cities, max_timepoints))  # Pad with zeros
+    out_ys = jnp.full(
+        shape=(n_cities, max_timepoints, n_variants), fill_value=1.0 / n_variants
+    )  # Pad with constant vectors
+
+    for i, (t, y) in enumerate(zip(ts, ys)):
+        n_timepoints = t.shape[0]
+
+        out_ts = out_ts.at[i, :n_timepoints].set(t)
+        out_ys = out_ys.at[i, :n_timepoints, :].set(y)
+        out_mask = out_mask.at[i, :n_timepoints].set(1)
+
+    return _ProblemData(
+        n_cities=n_cities,
+        n_variants=n_variants,
+        ts=out_ts,
+        ys=out_ys,
+        mask=out_mask,
+        n_quasimul=out_n,
+        overdispersion=out_overdispersion,
+    )
+
+
+def _quasiloglikelihood_single_city(
+    relative_growths: Float[Array, " variants-1"],
+    relative_offsets: Float[Array, " variants-1"],
+    ts: Float[Array, " timepoints"],
+    ys: Float[Array, "timepoints variants"],
+    mask: Float[Array, " timepoints"],
+    n_quasimul: float,
+    overdispersion: float,
+) -> float:
+    weight = n_quasimul / overdispersion
+    logps = calculate_logps(
+        ts=ts,
+        midpoints=_add_first_variant(relative_offsets),
+        growths=_add_first_variant(relative_growths),
+    )
+    return jnp.sum(mask[:, None] * weight * ys * logps)
+
+
+_RelativeGrowthsAndOffsetsFunction = Callable[
+    [Float[Array, " variants-1"], Float[Array, " variants-1"]], _Float
+]
+
+
+def _generate_quasiloglikelihood_function(
+    data: _ProblemData,
+) -> _RelativeGrowthsAndOffsetsFunction:
+    """Creates the quasilikelihood function with signature:
+
+    def quasiloglikelihood(
+        relative_growths: array of shape (variants-1,)
+        relative_offsets: array of shape (cities, variants-1)
+    ) -> float
+    """
+
+    def quasiloglikelihood(
+        relative_growths: Float[Array, " variants-1"],
+        relative_offsets: Float[Array, "cities variants-1"],
+    ) -> _Float:
+        # Broadcast the array, to use the same relative growths
+        # for each city
+        _new_shape = (data.n_cities, relative_growths.shape[-1])
+        tiled_growths = jnp.broadcast_to(relative_growths, _new_shape)
+
+        logps = jax.vmap(_quasiloglikelihood_single_city)(
+            relative_growths=tiled_growths,
+            relative_offsets=relative_offsets,
+            ts=data.ts,
+            ys=data.ys,
+            mask=data.mask,
+            n_quasimul=data.n_quasimul,
+            overdispersion=data.overdispersion,
+        )
+        return jnp.sum(logps)
+
+    return quasiloglikelihood
+
+
+def construct_model(
+    ys: list[jax.Array],
+    ts: list[jax.Array],
+    ns: Float[Array, " cities"] | list[float] | float = 1.0,
+    overdispersion: Float[Array, " cities"] | list[float] | float = 1.0,
+    sigma_growth: float = 10.0,
+    sigma_offset: float = 1000.0,
+) -> Callable:
+    """Builds a NumPyro model suitable for sampling from the quasiposterior.
+
+    Args:
+        ys: list of variant proportions for each city.
+            The ith entry should be an array
+            of shape (n_timepoints[i], n_variants)
+        ts: list of timepoints. The ith entry should be an array
+            of shape (n_timepoints[i],)
+            Note: `ts` should be appropriately normalized
+        ns: controls the overdispersion of each city by means of
+            quasimultinomial sample size
+        overdispersion: controls the overdispersion factor as in the
+            quasilikelihood approach
+        sigma_growth: controls the standard deviation of the prior
+            on the relative growths
+        sigma_offset: controls the standard deviation of the prior
+            on the relative offsets
+
+    Note:
+        The "loglikelihood" is effectively rescaled by `ns/overdispersion`
+        factor. Hence, using both `ns` and `overdispersion` should generally
+        be avoided.
+    """
+    data = _validate_and_pad(
+        ys=ys,
+        ts=ts,
+        ns_quasimul=ns,
+        overdispersion=overdispersion,
+    )
+
+    quasi_ll_fn = _generate_quasiloglikelihood_function(data)
+
+    def model():
+        # Sample growth differences. Note that we sample from the N(0, 1)
+        # distribution and then resample, for numerical stability
+        _scaled_rel_growths = numpyro.sample(
+            "_scaled_relative_growths",
+            distrib.Normal().expand((data.n_variants - 1,)),
+        )
+        rel_growths = numpyro.deterministic(
+            "relative_growths",
+            sigma_growth * _scaled_rel_growths,
+        )
+
+        # Sample offsets. We use scaling the same scaling trick as above
+        _scaled_rel_offsets = numpyro.sample(
+            "_scaled_relative_offsets",
+            distrib.Normal().expand((data.n_cities, data.n_variants - 1)),
+        )
+        rel_offsets = numpyro.deterministic(
+            "relative_offsets",
+            _scaled_rel_offsets * sigma_offset,
+        )
+
+        numpyro.factor("quasiloglikelihood", quasi_ll_fn(rel_growths, rel_offsets))
+
+    return model
+
+
+def construct_total_loss(
+    ys: list[jax.Array],
+    ts: list[jax.Array],
+    ns: list[float] | float = 1.0,
+    overdispersion: list[float] | float = 1.0,
+    accept_theta: bool = True,
+    average_loss: bool = False,
+) -> Callable[[_ThetaType], _Float] | _RelativeGrowthsAndOffsetsFunction:
+    """Constructs the loss function, suitable e.g., for optimization.
+
+    Args:
+        ys: list of variant proportions for each city.
+            The ith entry should be an array
+            of shape (n_timepoints[i], n_variants)
+        ts: list of timepoints. The ith entry should be an array
+            of shape (n_timepoints[i],)
+            Note: `ts` should be appropriately normalized
+        ns: controls the overdispersion of each city by means of
+            quasimultinomial sample size
+        overdispersion: controls the overdispersion factor as in the
+            quasilikelihood approach
+        accept_theta: whether the returned loss function should accept the
+            `theta` vector (suitable for optimization)
+            or should be parameterized by the relative growths
+            and relative offsets, as in
+            ```
+            def loss(
+                relative_growths: array of shape (variants-1,)
+                relative_offsets: array of shape (cities, variants-1)
+            ) -> float
+            ```
+        average_loss: whether the loss should be divided by the
+            total number of points. By default it is false, as the loss
+            is used to calculate confidence intervals. Setting it to true
+            can improve the convergence of the optimization procedure
+
+    Note:
+        The "loglikelihood" is effectively rescaled by `ns/overdispersion`
+        factor. Hence, using both `ns` and `overdispersion` should generally
+        be avoided.
+    """
+
+    data: _ProblemData = _validate_and_pad(
+        ys=ys,
+        ts=ts,
+        ns_quasimul=ns,
+        overdispersion=overdispersion,
+    )
+
+    if average_loss:
+        scaling = jnp.sum(data.mask, dtype=float)
+    else:
+        scaling = 1.0
+
+    # Get the quasilikelihood function
+    quasi_ll_fn = _generate_quasiloglikelihood_function(data)
+
+    # Define the loss function parameterized
+    # with relative growths and offsets
+    def _loss_fn(relative_growths, relative_offsets):
+        return -quasi_ll_fn(relative_growths, relative_offsets) / scaling
+
+    # Define the loss function in terms of the theta variable
+    def _loss_fn_theta(theta):
+        rel_growths = get_relative_growths(theta, n_variants=data.n_variants)
+        rel_offsets = get_relative_midpoints(theta, n_variants=data.n_variants)
+        return _loss_fn(relative_growths=rel_growths, relative_offsets=rel_offsets)
+
+    if accept_theta:
+        return _loss_fn_theta
+    else:
+        return _loss_fn
