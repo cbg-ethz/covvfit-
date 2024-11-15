20e1a154503177fb2db6b7a2fb03657830cd4b19
Paweł Czyż <pczyz@protonmail.com>
2024-11-15 12:56:13 +0100

    Finish the parser prototype

--- src/covvfit/_quasimultinomial.py
+++ src/covvfit/_quasimultinomial.py
@@ -438,11 +438,16 @@ _OverDispersionType = (
 )
 
 
+def _is_scalar(value) -> bool:
+    return not hasattr(value, "__len__")
+
+
 def _create_padded_array(
     values: _OverDispersionType,
     expected_lengths: list[int],
     padding_length: int,
     padding_value: float,
+    _out_dtype=float,
 ) -> Float[Array, "n_cities padding_length"]:
     n_cities = len(expected_lengths)
     if n_cities < 1:
@@ -452,24 +457,34 @@ def _create_padded_array(
             f"Maximum length is {max(expected_lengths)}, which is greater than the padding {padding_length}."
         )
 
-    out_array = jnp.full(shape=(n_cities, padding_length), fill_value=padding_value)
+    out_array = jnp.full(
+        shape=(n_cities, padding_length), fill_value=padding_value, dtype=_out_dtype
+    )
 
     # First case: `values` argument is a single number (not an iterable)
-    if not hasattr(values, "__len__"):
+    if _is_scalar(values):
         for i, length in enumerate(expected_lengths):
             out_array = out_array.at[i, :length].set(values)
         return out_array
 
-    # Second case: `values` argument is an iterable
+    # Second case: `values` argument is not a scalar, but rather an iterable:
     if len(values) != n_cities:
         raise ValueError(
             f"Provided list has length {len(values)} rather than {n_cities}."
         )
 
     for i, (value, exp_len) in enumerate(zip(values, expected_lengths)):
-        pass
+        if _is_scalar(value):  # For this city we have constant value provided
+            out_array = out_array.at[i, :exp_len].set(value)
+        else:  # We have a vector of values provided
+            if len(value) != exp_len:
+                raise ValueError(
+                    f"For {i}th component the provided array has length {len(value)} rather than {exp_len}."
+                )
+            vals = jnp.asarray(value, dtype=out_array.dtype)
+            out_array = out_array.at[i, :exp_len].set(vals)
 
-    raise NotImplementedError
+    return out_array
 
 
 def _validate_and_pad(
