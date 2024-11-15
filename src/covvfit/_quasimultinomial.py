dc07224bb0d5056644a4723e17eb72aac1d6a830
Paweł Czyż <pczyz@protonmail.com>
2024-11-15 13:58:37 +0100

    Move array padding utility to a different file

--- src/covvfit/_quasimultinomial.py
+++ src/covvfit/_quasimultinomial.py
@@ -11,6 +11,8 @@ import numpyro.distributions as distrib
 from jaxtyping import Array, Bool, Float
 from scipy import optimize
 
+from covvfit._padding import create_padded_array
+
 
 def calculate_linear(
     ts: Float[Array, " *batch"],
@@ -438,55 +440,6 @@ _OverDispersionType = (
 )
 
 
-def _is_scalar(value) -> bool:
-    return not hasattr(value, "__len__")
-
-
-def _create_padded_array(
-    values: _OverDispersionType,
-    expected_lengths: list[int],
-    padding_length: int,
-    padding_value: float,
-    _out_dtype=float,
-) -> Float[Array, "n_cities padding_length"]:
-    n_cities = len(expected_lengths)
-    if n_cities < 1:
-        raise ValueError("There has to be at least one city.")
-    if max(expected_lengths) > padding_length:
-        raise ValueError(
-            f"Maximum length is {max(expected_lengths)}, which is greater than the padding {padding_length}."
-        )
-
-    out_array = jnp.full(
-        shape=(n_cities, padding_length), fill_value=padding_value, dtype=_out_dtype
-    )
-
-    # First case: `values` argument is a single number (not an iterable)
-    if _is_scalar(values):
-        for i, length in enumerate(expected_lengths):
-            out_array = out_array.at[i, :length].set(values)
-        return out_array
-
-    # Second case: `values` argument is not a scalar, but rather an iterable:
-    if len(values) != n_cities:
-        raise ValueError(
-            f"Provided list has length {len(values)} rather than {n_cities}."
-        )
-
-    for i, (value, exp_len) in enumerate(zip(values, expected_lengths)):
-        if _is_scalar(value):  # For this city we have constant value provided
-            out_array = out_array.at[i, :exp_len].set(value)
-        else:  # We have a vector of values provided
-            if len(value) != exp_len:
-                raise ValueError(
-                    f"For {i}th component the provided array has length {len(value)} rather than {exp_len}."
-                )
-            vals = jnp.asarray(value, dtype=out_array.dtype)
-            out_array = out_array.at[i, :exp_len].set(vals)
-
-    return out_array
-
-
 def _validate_and_pad(
     ys: list[jax.Array],
     ts: list[jax.Array],
@@ -528,29 +481,29 @@ def _validate_and_pad(
         max_timepoints = max(max_timepoints, t.shape[0])
 
     _lengths = [t.shape[0] for t in ts]
-    out_n = _create_padded_array(
+    out_n = create_padded_array(
         values=ns_quasimul,
-        expected_lengths=_lengths,
+        lengths=_lengths,
         padding_length=max_timepoints,
         padding_value=0.0,
     )
-    out_overdispersion = _create_padded_array(
+    out_overdispersion = create_padded_array(
         values=overdispersion,
-        expected_lengths=_lengths,
+        lengths=_lengths,
         padding_length=max_timepoints,
         padding_value=1.0,  # Use 1.0 as we divide by it and want to avoid NaNs
     )
 
     # Now create the arrays representing the data
-    out_ts = _create_padded_array(
+    out_ts = create_padded_array(
         values=ts,
-        expected_lengths=_lengths,
+        lengths=_lengths,
         padding_length=max_timepoints,
         padding_value=0.0,
     )
-    out_mask = _create_padded_array(
+    out_mask = create_padded_array(
         values=1,
-        expected_lengths=_lengths,
+        lengths=_lengths,
         padding_length=max_timepoints,
         padding_value=0,
         _out_dtype=bool,
