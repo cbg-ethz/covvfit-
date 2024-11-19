b20b75404386892a253d03e93ba083f2430a6b8a
Paweł Czyż <pczyz@protonmail.com>
2024-11-19 10:17:58 +0100

    Adjust public API

--- src/covvfit/plotting/__init__.py
+++ src/covvfit/plotting/__init__.py
@@ -1,8 +1,9 @@
 """Plotting functionalities."""
 
+import covvfit.plotting._timeseries as timeseries
 from covvfit.plotting._grid import plot_grid, set_axis_off
 from covvfit.plotting._simplex import plot_on_simplex
-from covvfit.plotting._timeseries import colors_covsp, make_legend, num_to_date
+from covvfit.plotting._timeseries import COLORS_COVSPECTRUM, make_legend, num_to_date
 
 __all__ = [
     "plot_on_simplex",
@@ -10,5 +11,6 @@ __all__ = [
     "set_axis_off",
     "make_legend",
     "num_to_date",
-    "colors_covsp",
+    "timeseries",
+    "COLORS_COVSPECTRUM",
 ]
