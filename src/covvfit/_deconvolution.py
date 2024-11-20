31315d4c139c9fa2ae8e6c21ef0790615d4e66ba
Paweł Czyż <pczyz@protonmail.com>
2024-11-20 18:59:50 +0100

    Prototype quasiloglikelihood calculation in a numerically stable manner

--- src/covvfit/_deconvolution.py
+++ src/covvfit/_deconvolution.py
@@ -29,19 +29,19 @@ class _DeconvolutionProblemData(NamedTuple):
 
 class LogisticGrowthParams(NamedTuple):
     relative_growths: Float[Array, " variants-1"]
-    relative_offsets: Float[Array, " variants-1"]
+    relative_offsets: Float[Array, "cities variants-1"]
 
 
 PyTree = TypeVar("PyTree")
 
 GrowthModel = Callable[
-    [PyTree, Float[Array, " timepoints"]], Float[Array, "timepoints variants"]
+    [PyTree, Float[Array, " timepoints"]], Float[Array, "cities timepoints variants"]
 ]
 
 
 def logistic_growth(
     params: LogisticGrowthParams, ts: Float[Array, " timepoints"]
-) -> Float[Array, "timepoints variants"]:
+) -> Float[Array, "cities timepoints variants"]:
     return qm.calculate_logps(
         ts=ts,
         midpoints=qm._add_first_variant(params.relative_offsets),
@@ -49,26 +49,39 @@ def logistic_growth(
     )
 
 
+def _log_abundance(
+    a: Float[Array, " *shape"],
+    threshold: float = 1e-7,
+) -> Float[Array, " *shape"]:
+    log_a = jnp.log(a)
+    neg_inf = jnp.finfo(a.dtype).min
+    return jnp.where(a > threshold, log_a, neg_inf)
+
+
+def log1mexp(x):
+    """Compute log(1 - exp(x)) in a numerically stable way."""
+    x = jnp.minimum(x, -jnp.finfo(x.dtype).eps)
+    return jnp.where(x > -0.693, jnp.log(-jnp.expm1(x)), jnp.log1p(-jnp.exp(x)))
+
+
 def _quasiloglikelihood_single_city(
-    growth_model: GrowthModel,
-    params: PyTree,
-    ts: Float[Array, " timepoints"],
+    log_abundance: Float[Array, "timepoints variants"],
+    log_variant_defs: Float[Array, "variants mutations"],
     ms: Float[Array, "timepoints mutations"],
     mask: Bool[Array, "timepoints mutations"],
     n_quasibin: Float[Array, "timepoints mutations"],
     overdispersion: Float[Array, "timepoints mutations"],
-    log_variant_defs: Float[Array, "variants mutations"],
 ) -> float:
-    # Generate relative variant abundances,
-    # shape (timepoints, variants)
-    log_variant_abundances = growth_model(params, ts)
-
     # Now generate the log-probability of
     # finding the mutation at each locus
     # with shape (timepoints, mutations)
     # Note that this is a quasibinomial model for each entry,
     # so that the sums obtained by `a.sum(axis=-1)`
     # can be as large as `mutations`, rather than 1.
-    jnp.einsum("tv,vm->tm", log_variant_abundances, log_variant_defs)
+    log_p = jnp.einsum("tv,vm->tm", log_abundance, log_variant_defs)
+    log1_minusp = log1mexp(log_p)
 
-    # Now calculate the quasiloglikelihood
+    log_quasi = (
+        mask * n_quasibin * (ms * log_p + (1.0 - ms) * log1_minusp) / overdispersion
+    )
+    return jnp.sum(log_quasi)
