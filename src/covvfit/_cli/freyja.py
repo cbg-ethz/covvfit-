0f9a7f9e20ec5beda1f79d18fd80df0b07cb9780
Paweł Czyż <pczyz@protonmail.com>
2025-01-20 20:06:40 +0100

    Minor refactoring

--- src/covvfit/_cli/freyja.py
+++ src/covvfit/_cli/freyja.py
@@ -4,11 +4,12 @@ from typing import Annotated
 import typer
 
 
-def gather(
+def freyja_gather(
     directory: Annotated[str, typer.Argument(help="Directory with Freyja output")],
     metadata: Annotated[str, typer.Argument(help="Metadata")],
     output: Annotated[str, typer.Argument(help="Desired output location")],
 ):
-    """Gathers files from the directory into the output CSV."""
+    """Gathers Freyja-demixed files from the given directory into the output CSV,
+    adding the metadata."""
     typer.echo(f"Processing data file: {directory}")
     # Add your processing logic here
--- src/covvfit/cli.py
+++ src/covvfit/cli.py
@@ -1,14 +1,19 @@
 """Main script to which subcommands from `covvfit._cli` can be added."""
 import typer
 
-from covvfit._cli.freyja import gather
+from covvfit._cli.freyja import freyja_gather
 from covvfit._cli.infer import infer
 
 app = typer.Typer()
 
-# Add scripts here
-app.command()(gather)
-app.command()(infer)
+
+def _add_script(fn):
+    app.command()(fn)
+
+
+# Add functions processinf the data:
+_add_script(freyja_gather)
+_add_script(infer)
 
 
 def main():
